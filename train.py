b8c27a39d8859f192cd4a49f9d95efe63a0a73ed
Jo Walsh <jowalsh@bgs.ac.uk>
2020-03-08 02:08:55 +0000

    pass thru result_dir to persistent storage location

--- train.py
+++ train.py
@@ -3,9 +3,6 @@ sys.path.append('stylegan2')
 sys.path.append('forambulator')
 import forams.train
 
-# Try setting us to persistent storage on paperspace
-os.chdir('/storage')
-
 # Restart from scratch
 # resume_from_filename = os.path.join(home, 'results/00035-stylegan2-tfrecords_cg-1gpu-config-f/network-snapshot-000216.pkl')
 resume_from_filename = None
@@ -15,4 +12,5 @@ forams.train.train(data_dir='.',
                    metrics=[],
                    total_kimg=3000,
                    resume_from=resume_from_filename,
+                   result_dir='/storage/coccoliths'
                    save_ticks=1)
